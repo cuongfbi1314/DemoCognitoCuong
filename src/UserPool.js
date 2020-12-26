1c7af282f7b923448cb75e2a5e353eaa1a6ef128
Nguyễn Văn Mạnh Cường <cuongfbi1314@gmail.com>
2020-12-26 23:42:37 +0700

    Revert "upsource"
    This reverts commit f66a528b38fd8f0c3033a05fbcaf97fb606925a9.

--- src/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-const poolData = {
-  UserPoolId: 'us-west-2_12BBCqOUc',
-  ClientId: '60jidnoi750gg6l6nr7niubv1m'
-};
-
-export default new CognitoUserPool(poolData);
