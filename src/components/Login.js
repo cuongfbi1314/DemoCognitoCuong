b1661959514d4764b235a8a920161820150dcfd9
Nguyễn Văn Mạnh Cường <cuongfbi1314@gmail.com>
2020-12-26 23:43:23 +0700

    upsource

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import { AccountContext } from './Accounts';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { authenticate } = useContext(AccountContext);
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    authenticate(email, password)
+      .then(data => {
+        console.log('Logged in!', data);
+      })
+      .catch(err => {
+        console.error('Failed to login!', err);
+      })
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={newEmail}
+          onChange={event => setNewEmail(event.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+};
