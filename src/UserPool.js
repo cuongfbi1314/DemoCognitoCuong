b3075656cd0d69e04ecdfeba992b0cd95cf55ccc
Nguyễn Văn Mạnh Cường <cuongfbi1314@gmail.com>
2020-12-26 23:54:43 +0700

    Update UserPool.js

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'us-west-2_12BBCqOUc',
-  ClientId: '60jidnoi750gg6l6nr7niubv1m'
+  UserPoolId: '',
+  ClientId: ''
 };
 
 export default new CognitoUserPool(poolData);
